af6a5eb51d55c7af1dbaa3fef4046bd85a8b36c1
timalo <44393499+timalo@users.noreply.github.com>
2024-07-12 11:27:43 +0300

    Moved clutter from main to module.

--- src/DOMHandler.js
+++ src/DOMHandler.js
@@ -1,5 +1,5 @@
 // const main = require("./main");
-// const script = require("./scripts");
+const script = require("./scripts");
 
 function renderBoards(player1, player2) {
   // In addition to handling classes etc. We could also add images to hit/missed boxes.
@@ -45,4 +45,66 @@ function renderBoards(player1, player2) {
   }
 }
 
-module.exports = { renderBoards }; // eslint complains about this when exporting just one function. There are more funcs coming
+function init(player1, player2) {
+  const gameBoard1 = document.getElementsByClassName("board1")[0];
+  const gameBoard2 = document.getElementsByClassName("board2")[0];
+  for (let i = 0; i < 10; i += 1) {
+    for (let j = 0; j < 10; j += 1) {
+      const box1 = document.createElement("div");
+      box1.classList.add("box");
+      box1.classList.add("playerUnhit");
+      box1.id = `box1-${i}${j}`;
+      // box1.addEventListener("click", () => hitShip(1, j, i, player1, player2));
+      // hehe, this is not needed for the CPU. Would need if implementing 2-player mode.
+      gameBoard1.appendChild(box1);
+      const box2 = document.createElement("div");
+      box2.classList.add("box");
+      // box2.classList.add("unhit");
+      box2.id = `box2-${i}${j}`;
+      box2.addEventListener("click", () => hitShip(2, j, i, player1, player2));
+      gameBoard2.appendChild(box2);
+    }
+  }
+  player1.toggleTurn(); // init the game by giving the turn to p1
+}
+
+function hitShip(boardID, x, y, p1, p2) {
+  console.log(`hit ${x}, ${y} on board ${boardID}`);
+  if (boardID === 2 && p1.currentTurn === true) {
+    const hitValidity = p2.gameBoard.receiveAttack(x, y);
+    console.log(hitValidity);
+    if (hitValidity[0] === false) {
+      console.log("illegal move.");
+    } else if (hitValidity[0] === true && hitValidity[1] === false) {
+      console.log("miss! Toggling turn to CPU...");
+      p1.toggleTurn();
+      p2.toggleTurn();
+      const attackCoords = script.CPUAttack();
+      console.log(`CPU attacks ${attackCoords[0]}, ${attackCoords[1]}`);
+      hitShip(1, attackCoords[0], attackCoords[1], p1, p2);
+    } else if (hitValidity[0] === true && hitValidity[1] === true) {
+      console.log("hit! Turn continues.");
+    }
+  } else if (boardID === 2) {
+    console.log("Not your turn!");
+  } else if (p2.currentTurn === true) {
+    const hitValidity = p1.gameBoard.receiveAttack(x, y);
+    if (hitValidity[0] === false) {
+      console.log("illegal move from CPU. Trying another one...");
+      const attackCoords = script.CPUAttack();
+      hitShip(1, attackCoords[0], attackCoords[1], p1, p2);
+    } else if (hitValidity[1] === false) {
+      console.log("miss! Toggling turn back to player");
+      p1.toggleTurn();
+      p2.toggleTurn();
+    } else if (hitValidity[0] === true && hitValidity[1] === true) {
+      console.log("hit! CPU turn continues.");
+      const attackCoords = script.CPUAttack();
+      hitShip(1, attackCoords[0], attackCoords[1], p1, p2);
+    }
+  }
+
+  renderBoards(p1, p2);
+}
+
+module.exports = { renderBoards, init }; // eslint complains about this when exporting just one function. There are more funcs coming
--- src/main.js
+++ src/main.js
@@ -30,67 +30,6 @@ window.onload = () => {
   player2.gameBoard.placeShip(1, 9, 8, "y");
   player2.gameBoard.placeShip(1, 0, 0, "x");
   player2.gameBoard.placeShip(1, 0, 2, "x");
-  init(player1, player2);
+  DOMHandler.init(player1, player2);
   DOMHandler.renderBoards(player1, player2);
 };
-
-function init(player1, player2) {
-  const gameBoard1 = document.getElementsByClassName("board1")[0];
-  const gameBoard2 = document.getElementsByClassName("board2")[0];
-  for (let i = 0; i < 10; i += 1) {
-    for (let j = 0; j < 10; j += 1) {
-      const box1 = document.createElement("div");
-      box1.classList.add("box");
-      box1.classList.add("playerUnhit");
-      box1.id = `box1-${i}${j}`;
-      // box1.addEventListener("click", () => hitShip(1, j, i, player1, player2));
-      gameBoard1.appendChild(box1);
-      const box2 = document.createElement("div");
-      box2.classList.add("box");
-      // box2.classList.add("unhit");
-      box2.id = `box2-${i}${j}`;
-      box2.addEventListener("click", () => hitShip(2, j, i, player1, player2));
-      gameBoard2.appendChild(box2);
-    }
-  }
-  player1.toggleTurn(); // init the game by giving the turn to p1
-}
-
-function hitShip(boardID, x, y, p1, p2) {
-  console.log(`hit ${x}, ${y} on board ${boardID}`);
-  if (boardID === 2 && p1.currentTurn === true) {
-    const hitValidity = p2.gameBoard.receiveAttack(x, y);
-    console.log(hitValidity);
-    if (hitValidity[0] === false) {
-      console.log("illegal move.");
-    } else if (hitValidity[0] === true && hitValidity[1] === false) {
-      console.log("miss! Toggling turn to CPU...");
-      p1.toggleTurn();
-      p2.toggleTurn();
-      const attackCoords = script.CPUAttack();
-      console.log(`CPU attacks ${attackCoords[0]}, ${attackCoords[1]}`);
-      hitShip(1, attackCoords[0], attackCoords[1], p1, p2);
-    } else if (hitValidity[0] === true && hitValidity[1] === true) {
-      console.log("hit! Turn continues.");
-    }
-  } else if (boardID === 2) {
-    console.log("Not your turn!");
-  } else if (p2.currentTurn === true) {
-    const hitValidity = p1.gameBoard.receiveAttack(x, y);
-    if (hitValidity[0] === false) {
-      console.log("illegal move from CPU. Trying another one...");
-      const attackCoords = script.CPUAttack();
-      hitShip(1, attackCoords[0], attackCoords[1], p1, p2);
-    } else if (hitValidity[1] === false) {
-      console.log("miss! Toggling turn back to player");
-      p1.toggleTurn();
-      p2.toggleTurn();
-    } else if (hitValidity[0] === true && hitValidity[1] === true) {
-      console.log("hit! CPU turn continues.");
-      const attackCoords = script.CPUAttack();
-      hitShip(1, attackCoords[0], attackCoords[1], p1, p2);
-    }
-  }
-
-  DOMHandler.renderBoards(p1, p2);
-}
